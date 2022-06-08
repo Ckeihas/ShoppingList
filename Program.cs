b61edbff6cbefebcdeddaabb7834edbbf771cfbd
Ckeihas <christian.keihas@outlook.com>
2022-06-08 10:40:53 +0300

    FirstTry

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace ShoppingList
 {
@@ -6,7 +8,24 @@ namespace ShoppingList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lisää tuote: ");
+            string input = Console.ReadLine();
+            string path = @"C:\users\ChristianKeihäs\Testi.txt";
+
+          
+        if(File.Exists(path))
+            {
+                string createText = input + Environment.NewLine;
+                File.WriteAllText(path, createText, Encoding.UTF8);
+               
+            }
+
+            string appendText = "NewLine" + Environment.NewLine;
+            File.AppendAllText(path, appendText, Encoding.UTF8);
+
+            string readText = File.ReadAllText(path);
+            Console.WriteLine(readText);
+           
         }
     }
 }
